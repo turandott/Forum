1531498d99e566de1fd958ce468593c943304be2
turandott <greyvilka@gmail.com>
2023-06-20 22:26:03 +0300

    Add reach text editor with Tiptap

---
+++ src/app/components/editor/RichTextEditor.tsx
@@ -0,0 +1,198 @@
+"use client"
+import { EditorContent, useEditor } from '@tiptap/react'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import StarterKit from '@tiptap/starter-kit'
+import React from 'react'
+import './styles.css'
+import { Color } from '@tiptap/extension-color'
+import ListItem from '@tiptap/extension-list-item'
+import TextStyle from '@tiptap/extension-text-style'
+import { Button } from '@mui/material'
+import {
+  FaBold,
+  FaHeading,
+  FaItalic,
+  FaListOl,
+  FaListUl,
+  FaQuoteLeft,
+  FaRedo,
+  FaStrikethrough,
+  FaUnderline,
+  FaUndo,
+} from "react-icons/fa"
+
+const MenuBar = ({ editor }) => {
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <div className="menuBar">
+      <div>
+        <Button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .toggleBold()
+              .run()
+          }
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+          <FaBold />
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .toggleItalic()
+              .run()
+          }
+          className={editor.isActive('italic') ? 'is-active' : ''}
+        >
+          <FaItalic />
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .toggleStrike()
+              .run()
+          }
+          className={editor.isActive('strike') ? 'is-active' : ''}
+        >
+          <FaStrikethrough />
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .toggleCode()
+              .run()
+          }
+          className={editor.isActive('code') ? 'is-active' : ''}
+        >
+          code
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+        >
+          <FaHeading />
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+        >
+          <FaHeading className="heading2"/>
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+        >
+          <FaHeading className="heading3" />
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          <FaListUl />
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive('orderedList') ? 'is-active' : ''}
+        >
+          <FaListOl />
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={editor.isActive('codeBlock') ? 'is-active' : ''}
+        >
+          code block
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive('blockquote') ? 'is-active' : ''}
+        >
+          <FaQuoteLeft />
+        </Button>
+        <Button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+          horizontal rule
+        </Button>
+        <Button onClick={() => editor.chain().focus().setHardBreak().run()}>
+          hard break
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .undo()
+              .run()
+          }
+        >
+          <FaUndo />
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .redo()
+              .run()
+          }
+        >
+          <FaRedo />
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+
+export default function RichTextEditor() {
+  const editor = useEditor({
+    extensions: [
+      Color.configure({ types: [TextStyle.name, ListItem.name] }),
+      TextStyle.configure({ types: [ListItem.name] }),
+      StarterKit.configure({
+        bulletList: {
+          keepMarks: true,
+          keepAttributes: true, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        },
+        orderedList: {
+          keepMarks: true,
+          keepAttributes: true, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        },
+      }),
+    ],
+    content: `
+          <h2>
+            Hi there, for new post
+          </h2>
+          <p>
+            this is a <em>new</em> post 
+          </p>
+          `
+  })
+
+  return (
+    <div className="max-w-[750px]">
+      <div className="textEditor">
+        <MenuBar editor={editor} />
+        <EditorContent editor={editor} className="h-[500px]" />
+      </div>
+    </div>
+  )
+}
---
+++ src/app/editor/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import RichTextEditor from '../components/editor/RichTextEditor'
+
+
+const EditorPage: FunctionComponent<Props> = ({ }) => {
+
+    return (
+        <div className="flex justify-center">
+            <RichTextEditor />
+        </div>
+    )
+}
+
+export default EditorPage
