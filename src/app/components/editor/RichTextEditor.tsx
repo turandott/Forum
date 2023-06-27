e4c4d233d72769ac045f1097c0c11e162c5281e9
turandott <greyvilka@gmail.com>
2023-06-27 17:21:09 +0300

    Devided menuBar and editor placheolder for text editor

--- src/app/components/editor/RichTextEditor.tsx
+++ src/app/components/editor/RichTextEditor.tsx
@@ -13,21 +13,9 @@ import './styles.scss'
 import { Color } from '@tiptap/extension-color'
 import ListItem from '@tiptap/extension-list-item'
 import TextStyle from '@tiptap/extension-text-style'
-import { Button } from '@mui/material'
-import {
-  FaBold,
-  FaHeading,
-  FaItalic,
-  FaListOl,
-  FaListUl,
-  FaQuoteLeft,
-  FaRedo,
-  FaStrikethrough,
-  FaUnderline,
-  FaUndo,
-} from "react-icons/fa"
 import { CustomButton } from '../buttons/buttons'
 import { createPost } from "../../api/api"
+import { MenuBar } from './MenuBar'
 
 const Title = Heading.extend({
   name: "title",
@@ -39,145 +27,6 @@ const DocumentWithTitle = Document.extend({
   content: "title block+",
 });
 
-const MenuBar = ({ editor }) => {
-  if (!editor) {
-    return null
-  }
-
-  return (
-    <div className="menuBar">
-      <div>
-        <Button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          disabled={
-            !editor.can()
-              .chain()
-              .focus()
-              .toggleBold()
-              .run()
-          }
-          className={editor.isActive('bold') ? 'is-active' : ''}
-        >
-          <FaBold />
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          disabled={
-            !editor.can()
-              .chain()
-              .focus()
-              .toggleItalic()
-              .run()
-          }
-          className={editor.isActive('italic') ? 'is-active' : ''}
-        >
-          <FaItalic />
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          disabled={
-            !editor.can()
-              .chain()
-              .focus()
-              .toggleStrike()
-              .run()
-          }
-          className={editor.isActive('strike') ? 'is-active' : ''}
-        >
-          <FaStrikethrough />
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleCode().run()}
-          disabled={
-            !editor.can()
-              .chain()
-              .focus()
-              .toggleCode()
-              .run()
-          }
-          className={editor.isActive('code') ? 'is-active' : ''}
-        >
-          code
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-          className={editor.isActive('heading1', { level: 1 }) ? 'is-active' : ''}
-        >
-          <FaHeading />
-          <span>1</span>
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-          className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
-        >
-          <FaHeading className="heading2" />
-          <span>2</span>
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-          className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
-        >
-          <FaHeading className="heading3" />
-          <span>3</span>
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={editor.isActive('bulletList') ? 'is-active' : ''}
-        >
-          <FaListUl />
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={editor.isActive('orderedList') ? 'is-active' : ''}
-        >
-          <FaListOl />
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-          className={editor.isActive('codeBlock') ? 'is-active' : ''}
-        >
-          code block
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={editor.isActive('blockquote') ? 'is-active' : ''}
-        >
-          <FaQuoteLeft />
-        </Button>
-        <Button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-          horizontal rule
-        </Button>
-        <Button onClick={() => editor.chain().focus().setHardBreak().run()}>
-          hard break
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().undo().run()}
-          disabled={
-            !editor.can()
-              .chain()
-              .focus()
-              .undo()
-              .run()
-          }
-        >
-          <FaUndo />
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().redo().run()}
-          disabled={
-            !editor.can()
-              .chain()
-              .focus()
-              .redo()
-              .run()
-          }
-        >
-          <FaRedo />
-        </Button>
-      </div>
-    </div>
-  )
-}
 
 
 export default function RichTextEditor() {
